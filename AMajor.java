e0d2b792114befae3447b1bd007f08184907112e
rokenny <ronanocionaodh@gmail.com>
2014-10-16 11:57:14 +0100

    Initial Commit

---
+++ AMajor.java
@@ -0,0 +1,63 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * @author Ronan Kenny
+ * @version 2.0 25/11/2013
+ * This class contains the A Major image and swaps images when selected or de-selected
+ */
+public class AMajor extends ShowChord
+{
+    public static int clicked = 0;
+    public static boolean ImageReader = false;
+    
+    public void act() 
+    {  
+        /** 
+         * Here the ImageReader is used to decide between normal image or selected image
+         */
+        if(ImageReader == false)
+        {
+            GreenfootImage img = new GreenfootImage("AMajor.png");
+            setImage(img);
+        }
+        else if(ImageReader == true)
+        {
+            GreenfootImage img = new GreenfootImage("ASelected.png");
+            setImage(img);
+        }
+        /** 
+         * Here the int clicked is used to change the ImageReader boolean value. Also it sets all
+         * other chord Imagereader values to false and Clicked values to 0 to return all the images
+         * to the de-selected images.
+         */
+        if (Greenfoot.mouseClicked(this) && (clicked % 2 == 0)) 
+        { 
+            ImageReader = true;
+            BMajor.ImageReader = false;
+            CMajor.ImageReader = false;
+            DMajor.ImageReader = false;
+            EMajor.ImageReader = false;
+            FMajor.ImageReader = false;
+            GMajor.ImageReader = false;
+            
+            clicked++;
+            BMajor.clicked = 0;
+            CMajor.clicked = 0;
+            DMajor.clicked = 0;
+            EMajor.clicked = 0;
+            FMajor.clicked = 0;
+            GMajor.clicked = 0;
+        }  
+        else if (Greenfoot.mouseClicked(this) && (clicked % 2 != 0)) 
+        {  
+            ImageReader = false;
+            clicked++;
+            BMajor.clicked = 0;
+            CMajor.clicked = 0;
+            DMajor.clicked = 0;
+            EMajor.clicked = 0;
+            FMajor.clicked = 0;
+            GMajor.clicked = 0;
+        } 
+    }   
+}
